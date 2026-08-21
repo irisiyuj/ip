b24e9ef640ff1a00ed49ec3838fb406be91cbc03
irisiyuj <irisiyujiang@gmail.com>
2026-08-22 00:05:11 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Irri.java
@@ -0,0 +1,19 @@
+public class Irri {
+    public static void main(String[] args) {
+        String banner =
+                " ___           _ \n"
+                        + "|_ _|_ __ _ __(_)\n"
+                        + " | || '__| '__| |\n"
+                        + " | || |  | |  | |\n"
+                        + "|___|_|  |_|  |_|\n";
+
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hi! I'm Irri.");
+        System.out.println("What's on your mind?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println("Bye. See you!");
+        System.out.println("____________________________________________________________");
+    }
+}
