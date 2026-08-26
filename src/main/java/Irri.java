85e96f20d84b9f7101b56fca1b9bf13fbc65e6c0
irisiyuj <irisiyujiang@gmail.com>
2026-08-26 23:45:18 +0800

    Level-1: echo

--- src/main/java/Irri.java
+++ src/main/java/Irri.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Irri {
     public static void main(String[] args) {
         String banner =
@@ -7,13 +8,26 @@ public class Irri {
                         + " | || |  | |  | |\n"
                         + "|___|_|  |_|  |_|\n";
 
+
         System.out.println("____________________________________________________________");
         System.out.println(banner);
         System.out.println("Hi! I'm Irri.");
         System.out.println("What's on your mind?");
         System.out.println("____________________________________________________________");
 
-        System.out.println("Bye. See you!");
-        System.out.println("____________________________________________________________");
+        Scanner in = new Scanner(System.in);
+        while(true){
+            String input = in.nextLine();
+            if (input.equalsIgnoreCase("bye")){
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. See you!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(": " + input);
+            System.out.println("____________________________________________________________");
+        }
+        in.close();
     }
 }
